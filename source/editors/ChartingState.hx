d06a26db538181dcf6eaa8a8582286d1dd1a66c3
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-07-02 01:05:57 +0300

    why

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -219,9 +219,9 @@ class ChartingState extends MusicBeatState
 			addSection();
 			PlayState.SONG = _song;
 		}
-		#if MODS_ALLOWED
+		#
+
 		Paths.destroyLoadedImages();
-		#end
 
 		#if desktop
 		// Updating Discord Rich Presence
